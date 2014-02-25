44404fbf42e3e21319255b9aee2c0892a856d731
yueye000 <13010701@qq.com>
2014-02-25 01:21:51 -0800

    monitor mm

--- public/include/config/monitor_crons.inc.php
+++ public/include/config/monitor_crons.inc.php
@@ -2,16 +2,19 @@
 
 // Small helper array that may be used on some page controllers to
 // fetch the crons we wish to monitor
-$aMonitorCrons = array('statistics','payouts','token_cleanup','archive_cleanup','blockupdate','findblock','notifications','tickerupdate','liquid_payout');
+$aMonitorCrons = array('statistics','payouts', 'payouts_mm','token_cleanup','archive_cleanup','blockupdate', 'blockupdate_mm','findblock', 'findblock_mm','notifications','tickerupdate','liquid_payout');
 
 switch ($config['payout_system']) {
 case 'pplns':
     $aMonitorCrons[] = $config['payout_system'] . '_payout';
+    $aMonitorCrons[] = $config['payout_system'] . '_payout_mm';
       break;
 case 'pps':
     $aMonitorCrons[] = $config['payout_system'] . '_payout';
+    $aMonitorCrons[] = $config['payout_system'] . '_payout_mm';
       break;
 case 'prop':
     $aMonitorCrons[] = 'proportional_payout';
+    $aMonitorCrons[] = 'proportional_payout_mm';
       break;
 }
